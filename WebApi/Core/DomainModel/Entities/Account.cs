7a00afea399bf48990af7eb12f12652fb171c6f3
rogallab <b-u.rogalla@ostfalia.de>
2024-05-18 17:15:27 +0200

    IBAN must be DEkk BBBB BBBB CCCC CCCC CC

--- WebApi/Core/DomainModel/Entities/Account.cs
+++ WebApi/Core/DomainModel/Entities/Account.cs
@@ -1,42 +1,35 @@
 using System;
 using WebApi.Core.DomainModel.NullEntities;
 using WebApi.Core.Dto;
+using WebApi.Core.Misc;
 namespace WebApi.Core.DomainModel.Entities;
 
 public class Account: AEntity {
+  
+   #region fields
+   private string _iban = string.Empty;
+   #endregion
    
    #region properties
    public override Guid Id { get; init; } = Guid.NewGuid();
-   public string Iban { get; init; } = string.Empty;
-   public double  Balance  { get; set; }
-   
+   public string Iban { 
+      get => _iban;
+      init => _iban = Utils.CheckIban(value);
+   }
+   public double Balance { get; set; } 
+
    // Navigation property
    public Owner Owner   { get; set; } = NullOwner.Instance;
    public Guid  OwnerId { get; set; } = NullOwner.Instance.Id;
    #endregion
    
    #region ctor
-   public Account() {
-      Iban = CheckIban(Iban);
-   }
+   public Account() { }
    public Account(AccountDto dto) {
       Id = dto.Id;
-      Iban = CheckIban(dto.Iban);
+      Iban = Utils.CheckIban(dto.Iban);
       Balance = dto.Balance;
       OwnerId = dto.OwnerId;
-   }
-   #endregion
-
-   #region methods
-   private string CheckIban(string iban) {
-      if (iban.Length >= 8) 
-         return iban;
-      var random = new Random();
-      var newIban = "DE" +
-         random.Next(10, 99).ToString() +
-         random.Next(10000000, 99999999).ToString() +
-         random.Next(10000000, 99999999).ToString();
-      return newIban;
    }
    #endregion
    
--- WebApi/Core/Dto/AccountDto.cs
+++ WebApi/Core/Dto/AccountDto.cs
@@ -3,9 +3,12 @@ namespace WebApi.Core.Dto;
 
 // immutable data class
 public record AccountDto(
-   Guid    Id,     
-   string  Iban,    
-   double  Balance, 
-   // Navigation property
-   Guid    OwnerId  
-);
+   Guid Id,
+   string Iban,
+   double Balance,
+   Guid OwnerId = default
+) {
+   public AccountDto() : this(Guid.Empty, string.Empty, 0.0) { 
+      Iban = Iban.Replace(" ", "").ToUpper();
+   } 
+};
